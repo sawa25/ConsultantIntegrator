ed9680b3fd2691dafe016bd34ea0c81df1d9a71e
Veronika <VGZhidyaevskaya@google.com>
2024-06-24 12:45:10 +0300

    VSTORE_теги h1,2,3...

--- vector_store.py
+++ vector_store.py
@@ -31,7 +31,7 @@ def langchain_docs_extractor(soup: BeautifulSoup) -> str:
                 yield child
             elif isinstance(child, Tag):
                 if child.name in ["h1", "h2", "h3", "h4", "h5", "h6"]:
-                    yield f"{'\n\n#' * int(child.name[1:])} {child.get_text()}\n\n"
+                    yield f"\n\n{'#' * int(child.name[1:])} {child.get_text()}\n\n"
                 elif child.name == "a":
                     yield f"[{child.get_text(strip=False)}]({child.get('href')})"
                 elif child.name == "img":
