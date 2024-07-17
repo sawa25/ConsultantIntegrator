5553a7207a5e35af4de41f8556663387a69438e8
nzhidovinov <n.zhidovinov@yandex.ru>
2024-07-17 20:06:24 +0300

    Update loader

--- nzhidovinov/loader.py
+++ nzhidovinov/loader.py
@@ -10,31 +10,25 @@ class UniversalLoader:
     def __init__(self, encoding='utf-8'):
         self.encoding = encoding
 
-    def load_from_urls(self, urls: list[str] = [], recursive=False, **kwargs) -> list[Document]:
+    def load_from_urls(self, urls: list[str], recursive=False, max_depth=8, timeout=60, **kwargs) -> list[Document]:
         """
         Parses provided URLs and return documents.
 
         :param urls: URLS to parse.
         :param recursive: Parse recursive starting from URLs.
+        :param max_depth: Maximum depth of recursion.
+        :param timeout: Page loading timeout.
         :param kwargs:
         :return: List of documents.
         """
-
-        def get_name(_url):
-            path_segments = urlparse(_url).path.rstrip('/').split('/')
-            return path_segments[-1]
-
-        def get_base_url(_url: str):
-            parts = _url.split('/')
-            return f'{parts[0]}//{parts[2]}'
-
+        
         documents = []
         for url in urls:
             loader = RecursiveUrlLoader(
                 url,
-                max_depth=8 if recursive else 1,
+                max_depth=max_depth if recursive else 1,
                 extractor=docs_extractor,
-                timeout=60,
+                timeout=timeout,
                 prevent_outside=True,
                 check_response_status=True,
                 continue_on_failure=True,
@@ -43,7 +37,8 @@ class UniversalLoader:
             docs = loader.load()
             # (Optional splits, checks goes here)
             documents.extend(docs)
-
+            
         return documents
 
-    # load_from_X methods ...from
+    # load_from_X methods ...
+    
